fcf7da3e2d60a397a08ecf41ed4fe5cb3e006eab
Cerebral <cerebrallolzy1@gmail.com>
2025-04-10 15:56:28 -0500

    Fix structure

--- Scripts/PopUpTarget.sqf
+++
@@ -1,3 +0,0 @@
-private ["_target"];
-_target = (_this select 0) select 0;
-_target setDamage 0;
--- config/subconfigs/performance.hpp
+++
@@ -1,5 +0,0 @@
-// setup game optimizations
-setviewdistance = 2000;
-setobjectviewdistance[] = {1700,100};
-setterraingrid = 30;
-enableenvironment[] = {0, 0};
